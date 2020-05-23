91cfcc7a924204e74ac894389e9affd1abc8a184
staticdev <thiagocavila@gmail.com>
2020-05-23 12:26:51 -0300

    Update template

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 project = "Pdf Split Tool"
-author = "Claudio Jolowicz"
+author = "Thiago Carvalho D'Ávila"
 copyright = f"{datetime.now().year}, {author}"
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 autodoc_typehints = "description"
