d6ce52649a9401bd9446264cfb85af4d244e584a
staticdev <thiagocavila@gmail.com>
2020-05-23 14:16:23 -0300

    Mypy fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,20 +1,23 @@
 """Test cases for the __main__ module."""
+from unittest.mock import Mock
 from unittest.mock import patch
 
+import click.testing
 import pytest
-from click.testing import CliRunner
 
 from pdf_split_tool import __main__
 
 
 @pytest.fixture
-def runner() -> CliRunner:
+def runner() -> click.testing.CliRunner:
     """Fixture for invoking command-line interfaces."""
-    return CliRunner()
+    return click.testing.CliRunner()
 
 
 @patch("pdf_split_tool.file_handler.get_filenames")
-def test_main_succeeds(mock_file_handler_get_filenames, runner: CliRunner) -> None:
+def test_main_succeeds(
+    mock_file_handler_get_filenames: Mock, runner: click.testing.CliRunner
+) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(__main__.main)
     assert result.exit_code == 0
