5beda1492eff87e8d7a3b4e953136eb1c47ba301
staticdev <thiagocavila@gmail.com>
2020-05-23 14:03:44 -0300

    Test fixes

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
 """Test cases for the __main__ module."""
+from unittest.mock import patch
+
 import pytest
 from click.testing import CliRunner
 
@@ -11,7 +13,8 @@ def runner() -> CliRunner:
     return CliRunner()
 
 
-def test_main_succeeds(runner: CliRunner) -> None:
+@patch("pdf_split_tool.file_handler.get_filenames")
+def test_main_succeeds(mock_file_handler_get_filenames, runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(__main__.main)
     assert result.exit_code == 0
