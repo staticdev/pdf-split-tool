f60f33e0a2dc82c4eb3099be13a3afeb802bea73
staticdev <thiagocavila@gmail.com>
2020-05-26 17:15:56 -0300

    Max-size functionality

--- src/pdf_split_tool/__main__.py
+++ src/pdf_split_tool/__main__.py
@@ -1,81 +1,39 @@
 """Command-line interface."""
-import os
-import sys
-import tempfile
-
 import click
-import PyPDF4
 
 import pdf_split_tool.file_handler
-
-
-def get_pdf_size(pdf_writer: PyPDF4.PdfFileWriter) -> int:
-    """Generates temporary PDF.
-
-    Args:
-        pdf_writer: pdf writer.
-
-    Returns:
-        int: generated file size.
-    """
-    with tempfile.TemporaryFile(mode="wb") as fp:
-        pdf_writer.write(fp)
-        return fp.tell()
+import pdf_split_tool.pdf_splitter
 
 
 @click.command()
 @click.version_option()
-def main() -> None:
+@click.option(
+    "-m",
+    "--max-size",
+    type=int,
+    help="Max size in megabytes.",
+    default=20,
+    show_default=True,
+)
+def main(max_size: int) -> None:
     """Pdf Split Tool."""
-    max_size = 20 * 1024 * 1024  # 20 MB
+    max_size_bytes = max_size * 1024 * 1024  # convert to MB
     filepaths = pdf_split_tool.file_handler.get_filenames("*.pdf")
     for filepath in filepaths:
-        size = os.path.getsize(filepath)
-        print("File: {}\nFile size: {}".format(filepath, size))
-        if size > max_size:
-            input_pdf = PyPDF4.PdfFileReader(filepath, "rb")
-            total_pages = input_pdf.getNumPages()
-            avg_size = size / total_pages
-            avg_step = int(max_size / avg_size)
-            print("Total pages: {}\nAverage size: {}".format(total_pages, avg_size))
-
-            # warn high resolution
-            if avg_size > 1024 * 200:
-                click.secho(
-                    (
-                        "Warning: {} has more than 200kb per page. "
-                        "Consider reducing resolution before splitting."
-                    ).format(filepath),
-                    fg="yellow",
-                )
-
-            pdfs_count = 0
-            current_page = 0
-            end_page = current_page + avg_step
-
-            while current_page != total_pages:
-                current_size = sys.maxsize
-
-                # while PDF is too big create smaller PDFs
-                while current_size > max_size:
-                    pdf_writer = PyPDF4.PdfFileWriter()
-                    for page in range(current_page, end_page):
-                        pdf_writer.addPage(input_pdf.getPage(page))
-                    current_size = get_pdf_size(pdf_writer)
-                    input_pdf = PyPDF4.PdfFileReader(filepath, "rb")
-                    end_page -= 1
-
-                # write PDF with first 20mb
-                with open(
-                    filepath.replace(".pdf", "-{}.pdf".format(pdfs_count)), "wb"
-                ) as out:
-                    pdf_writer.write(out)
-
-                current_page = end_page + 1
-                end_page += avg_step
-                if end_page > total_pages:
-                    end_page = total_pages
-                pdfs_count += 1
+        splitter = pdf_split_tool.pdf_splitter.PdfSplitter(filepath)
+        valid = splitter.validate_resolution()
+        if not valid:
+            click.secho(
+                (
+                    "Warning: {} has more than 200kb per page. "
+                    "Consider reducing resolution before splitting."
+                ).format(filepath),
+                fg="yellow",
+            )
+            if not click.confirm("Do you want to continue?"):
+                click.secho("{} skipped.".format(filepath), fg="blue")
+                continue
+        splitter.split_max_size(max_size_bytes)
 
 
 if __name__ == "__main__":
---
+++ src/pdf_split_tool/pdf_splitter.py
@@ -0,0 +1,79 @@
+"""PDF operations module."""
+import os
+import sys
+import tempfile
+
+import PyPDF4
+
+
+class PdfSplitter:
+    """Pdf Splitter class."""
+
+    def __init__(self, filepath: str) -> None:
+        """Constructor."""
+        self.filepath = filepath
+        self.input_pdf = PyPDF4.PdfFileReader(filepath, "rb")
+        self.total_pages = self.input_pdf.getNumPages()
+        self.size = os.path.getsize(filepath)
+        self.avg_size = self.size / self.total_pages
+        print(
+            "File: {}\nFile size: {}\nTotal pages: {}\nAverage size: {}".format(
+                filepath, self.size, self.total_pages, self.avg_size
+            )
+        )
+
+    def validate_resolution(self) -> bool:
+        """Checks if resolution is too high."""
+        if self.avg_size > 1024 * 200:
+            return False
+        return True
+
+    def get_pdf_size(self, pdf_writer: PyPDF4.PdfFileWriter) -> int:
+        """Generates temporary PDF.
+
+        Args:
+            pdf_writer: pdf writer.
+
+        Returns:
+            int: generated file size.
+        """
+        with tempfile.TemporaryFile(mode="wb") as fp:
+            pdf_writer.write(fp)
+            return fp.tell()
+
+    def split_max_size(self, max_size: int) -> None:
+        """Creates new files based on max size.
+
+        Args:
+            max_size: size in integer megabytes.
+        """
+        if self.size > max_size:
+            avg_step = int(max_size / self.avg_size)
+
+            pdfs_count = 0
+            current_page = 0
+            end_page = current_page + avg_step
+
+            while current_page != self.total_pages:
+                current_size = sys.maxsize
+
+                # while PDF is too big create smaller PDFs
+                while current_size > max_size:
+                    pdf_writer = PyPDF4.PdfFileWriter()
+                    for page in range(current_page, end_page):
+                        pdf_writer.addPage(self.input_pdf.getPage(page))
+                    current_size = self.get_pdf_size(pdf_writer)
+                    self.input_pdf = PyPDF4.PdfFileReader(self.filepath, "rb")
+                    end_page -= 1
+
+                # write PDF with first 20mb
+                with open(
+                    self.filepath.replace(".pdf", "-{}.pdf".format(pdfs_count)), "wb"
+                ) as out:
+                    pdf_writer.write(out)
+
+                current_page = end_page + 1
+                end_page += avg_step
+                if end_page > self.total_pages:
+                    end_page = self.total_pages
+                pdfs_count += 1
